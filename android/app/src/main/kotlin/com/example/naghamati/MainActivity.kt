4695d1638773e82467b055681d0f1a1c7d9c1624
Mirna Hasan <mirnamhasan50@gmail.com>
2024-08-03 23:01:42 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/naghamati/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.naghamati
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+import 'package:just_audio/just_audio.dart';
+
+
+
+
+void main(){
+  runApp(const Music());
+}
+class Music extends StatefulWidget {
+  const Music ({super.key});
+
+  @override
+  State<Music> createState() => _MusicState();
+}
+
+class _MusicState extends State<Music> {
+   late AudioPlayer player = AudioPlayer();
+    @override
+  void initState() {
+    super.initState();
+
+   
+    player = AudioPlayer();
+
+ 
+    player.setReleaseMode(ReleaseMode.stop);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await player.setSource(AssetSource('songs/song1.mp3'),);
+      await player.resume();
+    });
+  }
+   @override
+  void dispose() {
+ 
+    player.dispose();
+
+    super.dispose();
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+   
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 216, 181, 218),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 191, 182, 193),
+          title: const Row(
+            children: [
+              Icon(Icons.queue_music , size: 35,color: Color.fromARGB(255, 156, 105, 166),), 
+              const Text("   Naghamati", style: TextStyle(
+                fontFamily: "EduAUVICWANTHand",
+                fontSize: 32, 
+                fontWeight: FontWeight.bold, 
+                color: Color.fromRGBO(103, 7, 85, 0.565),
+              ),) , 
+           
+              Text(" App", style: TextStyle(
+                 fontFamily: "EduAUVICWANTHand",
+                fontSize: 30, 
+                fontWeight: FontWeight.bold, 
+                color: Color.fromARGB(144, 119, 21, 83),
+              ),) , 
+            ],
+          )
+        ),
+        body: Column(
+          children: [
+            MaterialButton( color: Colors.white,
+              onPressed: (){
+               
+            
+              
+
+            }, 
+            child: Text("my music "),)
+          ],
+        )
+      ),
+    );
+  }
+}
